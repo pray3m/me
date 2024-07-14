fae30441ff45c20957e433d6a06d38df3860d71e
pray3m <prem.gtm9@gmail.com>
2024-07-14 20:11:36 +0545

    ✨feat(status) : add status to profile

--- common/components/elements/Status.tsx
+++ common/components/elements/Status.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { FC } from "react";
+import { motion } from "framer-motion";
 
-const Status = () => {
-  return <div>Status</div>;
+const Status: FC = () => {
+  return (
+    <div className="flex items-center gap-2 pb-2">
+      <motion.div
+        className="h-2 w-2 bg-green-600 rounded-full"
+        animate={{
+          scale: [1, 1.2, 1],
+        }}
+        transition={{
+          duration: 0.5,
+          repeat: Infinity,
+        }}
+      />
+      <span className="font-medium text-sm animate-pulse">
+        available for hire
+      </span>
+    </div>
+  );
 };
 
 export default Status;
