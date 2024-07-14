d75bf0d965216bcfca13432d57c7eb30b97c7288
pray3m <prem.gtm9@gmail.com>
2024-07-14 22:40:27 +0545

    💄styles : improve the dark mode toggle

--- common/components/elements/ToggleMode.tsx
+++ common/components/elements/ToggleMode.tsx
@@ -7,17 +7,17 @@ interface ToggleModeProps {
 }
 
 const ToggleMode: FC<ToggleModeProps> = ({ isDarkMode, onToggleChange }) => {
-  const switchText = "Switch to";
+  const switchText = "Switch to ";
   return (
     <button
       onClick={onToggleChange}
-      className="flex items-center gap-2 py-2 px-3 w-full lg:hover:dark:bg-zinc-800 lg:hover:bg-gray-200 lg:hover:rounded-lg lg:hover:scale-105 lg:transition-all lg:duration-300 "
+      className="flex items-center gap-2 py-2 px-3 w-full lg:hover:dark:bg-zinc-800 lg:hover:bg-gray-200 lg:hover:rounded-lg lg:hover:scale-105 lg:transition-all lg:duration-300"
     >
       <Icon glyph={isDarkMode ? "idea" : "moon-fill"} size={22} />
-      <div className="flex">
-        <span>{switchText}</span>
-        <span>{isDarkMode ? "Light Mode" : "Dark Mode"}</span>
-      </div>
+      <p className="flex">
+        <span className="hidden 2xl:block xl:mr-1">{switchText}</span>
+        {isDarkMode ? "Light Mode" : "Dark Mode"}
+      </p>
     </button>
   );
 };
