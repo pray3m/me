911cddc935bde22b97f3377b2711861f24a17cf0
pray3m <prem.gtm9@gmail.com>
2024-11-23 15:15:22 +0545

    feat(about): enhance CareerCard component with duration display and layout adjustments

--- modules/about/components/CareerCard.tsx
+++ modules/about/components/CareerCard.tsx
@@ -17,8 +17,21 @@ const CareerCard: FC<CareerProps> = ({
   industry,
   link,
 }) => {
+  const startDate = moment(start_date);
+  const endDate = end_date ? moment(end_date) : moment();
+
+  const durationYears = endDate.diff(startDate, "years");
+  const durationMonths = endDate.diff(startDate, "months") % 12;
+
+  let durationText = "";
+  if (durationYears > 0) {
+    durationText = `${durationYears} year${durationYears > 1 ? "s" : ""}`;
+  } else {
+    durationText = `${durationMonths} month${durationMonths > 1 ? "s" : ""}`;
+  }
+
   return (
-    <div className="flex items-center gap-5 py-4 px-5 border border-neutral-300 dark:border-neutral-700 rounded-xl transition-all duration-300">
+    <div className="flex items-center gap-5 py-4 px-6 border border-neutral-300 dark:border-neutral-700 rounded-xl transition-all duration-300 ">
       {logo ? (
         <Image src={logo} width={50} height={50} alt={company} />
       ) : (
@@ -27,7 +40,7 @@ const CareerCard: FC<CareerProps> = ({
 
       <div className="space-y-1">
         <h6> {position}</h6>
-        <div className=" text-sm text-neutral-600 dark:text-neutral-400 space-y-1">
+        <div className=" text-sm text-neutral-600 dark:text-neutral-400 space-y-2">
           <div className=" flex items-center gap-1 md:gap-2">
             <Link href={link || "#"} target="_blank">
               <span className="underline cursor-pointer hover:text-dark hover:dark:text-white">
@@ -38,16 +51,14 @@ const CareerCard: FC<CareerProps> = ({
             <span>{location}</span>
           </div>
 
-          <div className="flex flex-col md:flex-row md:items-center gap-1 md:gap-2  md:text-[13px] ">
+          <div className="flex flex-col md:text-[13px] ">
             <div className="flex gap-1">
-              <span>{moment(start_date).format("MMM YYYY")} </span> -{" "}
-              <span>
-                {end_date ? moment(end_date).format("MMM YYYY") : "Present"}
-              </span>
+              <span>{startDate.format("MMM YYYY")}</span> -{" "}
+              <span>{end_date ? endDate.format("MMM YYYY") : "Present"}</span>
             </div>
 
-            <span className="hidden md:block text-neutral-300 dark:text-neutral-700">
-              •
+            <span className=" text-neutral-500 dark:text-neutral-500">
+              ~ {durationText}
             </span>
             <span>{location}</span>
           </div>
