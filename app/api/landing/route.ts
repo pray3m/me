4bde63143deeb87513888ceec5c81cf7ed3974b9
pray3m <prem.gtm9@gmail.com>
2024-12-28 22:28:00 +0545

    feat(api): implement landing route and data fetching service

---
+++ app/api/landing/route.ts
@@ -0,0 +1,22 @@
+import { getApiData } from "@/services/pray3m";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const query = req.nextUrl.searchParams.get("query") || "";
+
+    const { status, data } = await getApiData(query);
+
+    return NextResponse.json(data, {
+      status,
+      headers: {
+        "Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+      },
+    });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Internal Server Error";
+
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
+  }
+}
