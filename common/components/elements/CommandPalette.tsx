6a8c82ccdafc3c863d32b842ed800aecda6d84eb
pray3m <prem.gtm9@gmail.com>
2025-04-05 14:50:51 +0545

    feat: enhance CommandPalette with theme switching and search functionality

--- common/components/elements/CommandPalette.tsx
+++ common/components/elements/CommandPalette.tsx
@@ -1,15 +1,26 @@
+import { MENU_ITEMS } from "@/common/constant/menu"
 import { CommandPaletteContext } from "@/common/context/CommandPaletteContext"
 import { MenuItemProps } from "@/common/lib/types"
 import {
   Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
   Dialog,
   DialogBackdrop,
-  DialogPanel,
   Transition,
   TransitionChild,
 } from "@headlessui/react"
+import clsx from "clsx"
+import { useTheme } from "next-themes"
 import { useRouter } from "next/navigation"
 import React, { Fragment, useContext, useEffect } from "react"
+import {
+  BiMoon as DarkModeIcon,
+  BiSun as LightModeIcon,
+  BiSearch as SearchIcon,
+} from "react-icons/bi"
+import { useDebounceValue } from "usehooks-ts"
 
 interface MenuOptionItemProps extends MenuItemProps {
   click?: () => void
@@ -25,6 +36,10 @@ export default function CommandPalette() {
   const router = useRouter()
 
   const { isOpen, setIsOpen } = useContext(CommandPaletteContext)
+  const [query, setQuery] = React.useState<string>("")
+  const [queryDebounce] = useDebounceValue(query, 300)
+
+  const { resolvedTheme, setTheme } = useTheme()
 
   useEffect(() => {
     const handleKeyDown: (event: KeyboardEvent) => void = (
@@ -38,7 +53,49 @@ export default function CommandPalette() {
     return () => window.removeEventListener("keydown", handleKeyDown)
   }, [isOpen, setIsOpen])
 
+  const menuOptions: MenuOptionProps[] = [
+    {
+      title: "THEME",
+      children: [
+        {
+          icon:
+            resolvedTheme === "dark" ? (
+              <LightModeIcon size={20} />
+            ) : (
+              <DarkModeIcon size={20} />
+            ),
+          title: `Switch to ${
+            resolvedTheme === "dark" ? "Light" : "Dark"
+          } Mode`,
+          click: () => setTheme(resolvedTheme === "dark" ? "light" : "dark"),
+          href: "#",
+          isExternal: false,
+          closeOnSelect: false,
+        },
+      ],
+    },
+    {
+      title: "PAGES",
+      children: MENU_ITEMS?.map((menu) => ({
+        ...menu,
+        closeOnSelect: true,
+      })),
+    },
+  ]
+
+  const filterMenuOptions: MenuOptionProps[] = queryDebounce
+    ? menuOptions.map((menu) => ({
+        ...menu,
+        children: menu.children.filter((item) =>
+          item.title.toLowerCase().includes(queryDebounce.toLowerCase())
+        ),
+      }))
+    : menuOptions
+
   const handleSelect = (menu: MenuOptionItemProps) => {
+    if (!menu) return
+
+    setQuery("")
     if (menu.closeOnSelect) setIsOpen(false)
 
     if (menu.click) {
@@ -52,6 +109,9 @@ export default function CommandPalette() {
       router.push(menu?.href as string)
     }
   }
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setQuery(event.target.value)
 
   return (
     <Transition show={isOpen} as={Fragment}>
@@ -77,7 +137,58 @@ export default function CommandPalette() {
             as="div"
             className="relative mx-auto max-w-lg overflow-hidden rounded-xl border-2 border-neutral-300 bg-white shadow-3xl ring-1 ring-black/5 dark:divide-neutral-600 dark:border-neutral-800  dark:bg-neutral-950 "
           >
-            <div>Command Palette is Open</div>
+            <div className="flex gap-3 items-center border-b border-neutral-300 dark:border-neutral-800 px-4">
+              <SearchIcon size={22} />
+              <ComboboxInput
+                autoFocus
+                onChange={handleSearch}
+                className="h-12 w-full border-0 bg-transparent text-neutral-800 placeholder-neutral-500 focus:outline-none focus:ring-0 dark:text-neutral-200"
+                placeholder="Press Cmd + K anytime to access this command palette"
+              />
+            </div>
+
+            <div className="max-h-80 overflow-y-auto py-2 px-1">
+              {filterMenuOptions.map((menu) => (
+                <div
+                  key={menu.title}
+                  className={clsx(
+                    menu.children.length === 0 && "hidden",
+                    "py-1"
+                  )}
+                >
+                  <div className="my-2 px-5 text-xs text-neutral-500">
+                    {menu?.title}
+                  </div>
+                  <ComboboxOptions static className="space-y-1">
+                    {menu.children.map((child) => (
+                      <ComboboxOption key={child.href} value={child}>
+                        {({ active }) => (
+                          <div
+                            className={clsx(
+                              active
+                                ? "bg-neutral-200 text-neutral-600 dark:bg-neutral-700/60 dark:text-white"
+                                : "text-neutral-600 dark:text-neutral-300",
+                              "mx-2 flex cursor-pointer items-center gap-3 rounded-md py-2 px-4"
+                            )}
+                          >
+                            {child?.icon && <span>{child?.icon}</span>}
+                            <span>{child?.title}</span>
+                          </div>
+                        )}
+                      </ComboboxOption>
+                    ))}
+                  </ComboboxOptions>
+                </div>
+              ))}
+            </div>
+
+            {queryDebounce &&
+              filterMenuOptions.flatMap((item) => item.children).length ===
+                0 && (
+                <p className="p-4 mb-4 text-sm text-neutral-500 text-center">
+                  No results found.
+                </p>
+              )}
           </Combobox>
         </TransitionChild>
       </Dialog>
