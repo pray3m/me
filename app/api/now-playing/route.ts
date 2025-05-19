a8fdc325863141cb70bb851fb5bcad3f39146acb
pray3m <prem.gtm9@gmail.com>
2025-05-20 01:07:00 +0545

    feat: add Spotify integration for now playing status and related types

---
+++ app/api/now-playing/route.ts
@@ -0,0 +1,25 @@
+import { getGithubUser } from "@/services/github"
+import { getNowPlaying } from "@/services/spotify"
+import { NextApiRequest, NextApiResponse } from "next"
+import { NextRequest, NextResponse } from "next/server"
+
+const CACHE_HEADERS = {
+  "Content-Type": "application/json",
+  "Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+}
+
+export async function GET(req: NextRequest) {
+  const response = await getNowPlaying()
+
+  if (response.status && response.status > 400) {
+    return NextResponse.json(
+      { error: "Failed to fetch data from Spotify." },
+      { status: response.status, headers: CACHE_HEADERS }
+    )
+  }
+
+  return NextResponse.json(response.data, {
+    headers: CACHE_HEADERS,
+    status: response.status,
+  })
+}
